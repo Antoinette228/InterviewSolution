c4403198ab5c092d52e73330c6e53f889ef3f47e
Antoinette228 <85918954+Antoinette228@users.noreply.github.com>
2021-06-15 08:28:08 -0700

    interview solution 2021-06-15

---
+++ TestingUnits/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestingUnits")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestingUnits")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6ee487e4-56eb-4dcd-9ce9-3d0dfe32e4b3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestingUnits/StackTesting.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections;
+using interviewTest;
+
+namespace TestingUnits
+{
+    [TestClass]
+    public class StackTesting
+    {
+        [TestMethod]
+        public void Testing_InputArraySize()
+        {           
+            TestingStack p = new TestingStack();
+
+            //Given
+            StringReader input = new StringReader("3");
+            Console.SetIn(input);
+
+            //When
+            int size = p.InputArraySize();
+
+            //Then
+            Assert.AreEqual(3, size, "Size should be 3"); 
+        }
+
+
+        [TestMethod]
+        public void Testing_PopulateStackValues()
+        {
+            TestingStack p = new TestingStack();            
+
+            //Given
+            StringReader input = new StringReader("95\n6\n7");
+            Console.SetIn(input);
+
+            //When 
+            Stack st = new Stack();
+            p.PopulateStackValues(3, st);
+
+            //Then
+            Assert.AreEqual(3, st.Count);  // .AreEqual(3, size, "Size should be 3");
+        }
+
+        [TestMethod]
+        public void Testing_Result()
+        {
+           /* TestingStack p = new TestingStack();
+
+            //Given
+            StringWriter output = new StringWriter();
+            Console.SetOut(output);
+
+            Stack input = new Stack();
+            input.Push(6);
+            input.Push(7);
+            input.Push(8);
+
+            //When            
+            p.Result(input);
+
+            //Then
+            Assert.AreEqual("6\n7\n8", output.ToString());  */
+        }
+    }
+}
